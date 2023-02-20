0ddd08941830e7ceeb1dec733e261f1f8b3c6df0
innocent <geesnell.nich45@gmail.com>
2023-02-20 20:31:50 +0300

    First Calc

--- Calculater/calc.dart
+++ Calculater/calc.dart
@@ -3,40 +3,43 @@ import 'dart:io';
 
 void main() {
   var op;
-  double a, b, c;
-  print('Enter the first Element: ');
-  a = double.parse(stdin.readLineSync());
-  print('Enter the sec Element :');
-  b = double.parse(stdin.readLineSync());
-  stdout.write(stdin.readLineSync());
-  print('------------------------------------');
+  int a, b, x;
+  print('------------------------------------------');
+  print('CALCULATOR FOR ARITHMETIC OPERATORS ');
+  print('------------------------------------------');
+  stdout.write('Enter the first Element: ');
+  a = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter the sec Element :');
+  b = int.parse(stdin.readLineSync()!);
+  print('-----------------------------------');
   print('Select the operation nos: ');
-  print('------------------------------------');
+  print('-----------------------------------');
   print('1.Additional');
   print('2.Subtraction');
   print('3.Multiplication');
   print('4.Division');
-  print('Get the remainder of an integers division');
-  op = stdin.readLineSync();
+  stdout.write('Enter the Value to perform the particular Operation :');
+
+  op = int.parse(stdin.readLineSync()!);
 
   switch (op) {
     case 1:
-      c = a + b;
+      x = a + b;
+      print("The addition of two no : $x");
       break;
     case 2:
-      c = a - b;
+      x = a - b;
+      print("The Subtration of two no : $x");
       break;
     case 3:
-      c = a * b;
+      x = a * b;
+      print("The Multiplication of two no : $x");
       break;
     case 4:
-      c = a / b;
-      break;
-    case 6:
-      c = a % b;
+      x = a % b;
+      print("The Division of Two no : $x");
       break;
     default:
       print('Wrong Choice Operators');
   }
-  print('$a,$b,$c,$op,');
 }
