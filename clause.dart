389e7a8c3013b5ffa019f191ad54f499ce0e671b
innocent <geesnell.nich45@gmail.com>
2023-02-26 01:06:24 +0300

    First proj

---
+++ clause.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+
+  stdout.write('Enter the first values of A: ');
+  var a=int.parse(stdin.readLineSync()!);
+  stdout.write('Enter the sec values of B: ');
+  var b=int.parse(stdin.readLineSync()!);
+  stdout.write('Enter the value of C: ');
+  var c =int.parse(stdin.readLineSync()!);
+  if (a > b) {
+    print('A is bigger : $a');
+  } else if (b > a) {
+    print('B is bigger :$b');
+  } else if (c > b) {
+    print('C is bigger :$c');
+  } else {
+    print('Nos is out of range');
+  }
+}
---
+++ if.dart
@@ -0,0 +1,9 @@
+void main() {
+  var age = 65;
+
+  if (age > 20) {
+    print('You are allowed to vote');
+  } else {
+    print('Your not allowed to vote');
+  }
+}
